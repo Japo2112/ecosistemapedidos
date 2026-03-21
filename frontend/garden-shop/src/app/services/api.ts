dbe1c7c786807562c94403a61f980ca8097a30f3
Japo <jpicheo@miumg.edu.gt>
2026-03-20 23:09:10 -0600

    Cambio frontend2

--- frontend/garden-shop/src/app/services/api.ts
+++ frontend/garden-shop/src/app/services/api.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Api {
-  
+export class ApiService {
+
+  private baseUrl = 'http://34.60.125.75:8000/api';
+
+  constructor(private http: HttpClient) {}
+
+  getProductos() {
+  return this.http.get(`/api/productos/listar`);
+}
 }
